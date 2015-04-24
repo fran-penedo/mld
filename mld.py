9c3730b302ee10bf2b99ea3ded5f92e42bbf3ef1
Francisco Penedo <fran.penedo@gmail.com>
2015-04-23 23:29:45 -0400

    Added max capacities to the model

--- mld.py
+++ mld.py
@@ -5,82 +5,92 @@ def label(name, i, j):
     return name + "_" + str(i) + "_" + str(j)
 
 
-def add_max_constr(m, label, args, K):
-    def delta_label(l, i):
-        return l + "_" + str(i)
+def delta_label(l, i):
+    return l + "_" + str(i)
 
+
+def add_max_constr(m, label, args, K):
     y = {}
     y[label] = m.addVar(lb=0, ub=K, name=label)
-    for i in range(len(args)):
-        l = delta_label(label, i)
-        y[l] = m.addVar(vtype=g.GRB.BINARY, name=l)
 
-    m.update()
+    if len(args) == 0:
+        m.update()
+        m.addConstr(y[label] == K)
 
-    for i in range(len(args)):
-        x = delta_label(label, i)
-        m.addConstr(y[label] >= args[i])
-        m.addConstr(y[label] <= args[i] + y[x] * K)
+    else:
+        for i in range(len(args)):
+            l = delta_label(label, i)
+            y[l] = m.addVar(vtype=g.GRB.BINARY, name=l)
 
-    m.addConstr(g.quicksum([y[delta_label(label, i)]
-                            for i in range(len(args))] == len(args) - 1))
+        m.update()
+
+        for i in range(len(args)):
+            x = delta_label(label, i)
+            m.addConstr(y[label] >= args[i])
+            m.addConstr(y[label] <= args[i] + y[x] * K)
+
+        m.addConstr(g.quicksum([y[delta_label(label, i)]
+                                for i in range(len(args))] == len(args) - 1))
 
     return y
 
 
 # TODO handle len(args) == 2 differently
 def add_min_constr(m, label, args, K):
-    def delta_label(l, i):
-        return l + "_" + str(i)
-
     y = {}
     y[label] = m.addVar(lb=0, ub=K, name=label)
-    for i in range(len(args)):
-        l = delta_label(label, i)
-        y[l] = m.addVar(vtype=g.GRB.BINARY, name=l)
 
-    m.update()
+    if len(args) == 0:
+        m.update()
+        m.addConstr(y[label] == K)
 
-    for i in range(len(args)):
-        x = delta_label(label, i)
-        m.addConstr(y[label] <= args[i])
-        m.addConstr(y[label] >= args[i] - y[x] * K)
+    else:
+        for i in range(len(args)):
+            l = delta_label(label, i)
+            y[l] = m.addVar(vtype=g.GRB.BINARY, name=l)
 
-    m.addConstr(g.quicksum([y[delta_label(label, i)]
-                            for i in range(len(args))]) == len(args) - 1)
+        m.update()
+
+        for i in range(len(args)):
+            x = delta_label(label, i)
+            m.addConstr(y[label] <= args[i])
+            m.addConstr(y[label] >= args[i] - y[x] * K)
+
+        m.addConstr(g.quicksum([y[delta_label(label, i)]
+                                for i in range(len(args))]) == len(args) - 1)
 
     return y
 
 
-def always(m, label, a, b, rho, K, t=0):
+def add_always_constr(m, label, a, b, rho, K, t=0):
     y = add_min_constr(m, label, rho[(t + a):(t + b + 1)], K)[label]
     # Just y>=0 (lower bound is part of the definition of the variable)
     return y
 
 
-def create_model(A, b, B, d, c, x0, cost, N):
-    # Big number required for y = min(x, c)
-    K = max(c) * 100
+def create_model(A, b, B, E, d, c, xcap, x0, cost, N):
+    # Big number for min alpha/beta (xcap - x)
+    K_out = max(xcap) / min([min([1] + [abs(i) for i in row if i > 0])
+                             for row in B])
+    # Big number required for y = min(x, c, min)
+    # No need to add bound on x because it's included in K_out
+    K = max(max(c), K_out) * 10
     # Bounds for x
     mm = 0
-    M = 10 * max(c)
+    M = max(xcap)
 
     m = g.Model("traffic")
 
     x = {}
     y = {}
-    #ymin = {}
     z = {}
     u = {}
+    v = {}
     for i in range(len(B)):
         for j in range(N):
             # actual decision variables
             # system state
             labelx = label("x", i, j)
-            # auxiliary variable y = min(x, c)
-            # labely = label("y", i, j)
-            # auxiliary binary variable needed to describe y
-            # labelymin = label("ymin", i, j)
             # auxiliary binary variable z = y * u
             labelz = label("z", i, j)
             # control variable
@@ -89,8 +99,6 @@ def create_model(A, b, B, d, c, x0, cost, N):
             x[labelx] = m.addVar(
                 obj=cost[i], lb=0, ub=g.GRB.INFINITY, name=labelx)
             if j < N - 1:
-                # y[labely] = m.addVar(lb=0, ub=c[i], name=labely)
-                # ymin[labelymin] = m.addVar(vtype=g.GRB.BINARY, name=labelymin)
                 z[labelz] = m.addVar(lb=0, ub=g.GRB.INFINITY, name=labelz)
                 u[labelu] = m.addVar(vtype=g.GRB.BINARY, name=labelu)
 
@@ -99,9 +107,17 @@ def create_model(A, b, B, d, c, x0, cost, N):
 
     for i in range(len(B)):
         for j in range(N):
+            # v = min(alpha/beta(xcap - x))
+            v.update(
+                add_min_constr(
+                    m, label("v", i, j),
+                    [E[k][i] / B[k][i] * (xcap[i] - x[label("x", i, j)])
+                     for k in range(len(B)) if B[k][i] > 0], K_out))
+            # y = min(x, c, v)
             y.update(
                 add_min_constr(
-                    m, label("y", i, j), [x[label("x", i, j)], c[i]], K))
+                    m, label("y", i, j),
+                    [x[label("x", i, j)], c[i], v[label("v", i, j)]], K))
 
     # Dynamics constraints
     for i in range(len(B)):
@@ -111,17 +127,13 @@ def create_model(A, b, B, d, c, x0, cost, N):
             else:
                 m.addConstr(x[label("x", i, j)] == x[label("x", i, j - 1)] +
                             d[i][j - 1] +
-                            g.quicksum(B[i][k] * z[label("z", k, j - 1)]
-                                       for k in range(len(B))))
+                            g.quicksum(
+                                E[i][k] * B[i][k] * z[label("z", k, j - 1)]
+                                for k in range(len(B))))
 
     # Transformation constraints
     for i in range(len(B)):
         for j in range(N - 1):
-            #m.addConstr(y[label("y", i, j)] <= x[label("x", i, j)])
-            # m.addConstr(
-            #    y[label("y", i, j)] >= x[label("x", i, j)] - K * ymin[label("ymin", i, j)])
-            # m.addConstr(
-            # y[label("y", i, j)] >= c[i] - K * (1 - ymin[label("ymin", i, j)]))
             m.addConstr(z[label("z", i, j)] <= M * u[label("u", i, j)])
             m.addConstr(z[label("z", i, j)] >= mm * u[label("u", i, j)])
             m.addConstr(
@@ -151,23 +163,28 @@ def run():
          [0, -1, 0, 0],
          [0.6, 0.5, -1, 0],
          [0, 0, 0, -1]]
+    E = [[1, 1, 1, 1],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1]]
     d = [[10, 0, 10, 0],
          [5, 5, 5, 5],
          [0, 0, 0, 0],
          [5, 5, 5, 5]]
     c = [20, 10, 20, 10]
+    xcap = [300, 300, 300, 300]
     x0 = [0, 0, 0, 0]
     cost = [2, 1, 2, 1]
     N = 5
 
-    m, var = create_model(A, b, B, d, c, x0, cost, N)
+    m, var = create_model(A, b, B, E, d, c, xcap, x0, cost, N)
     u = var[0]
     x = var[1]
     z = var[2]
     y = var[3]
     #ymin = var[4]
 
-    always(
+    add_always_constr(
         m, "alw", 0, 3, [-x[label("x", 3, j)] + 9 for j in range(N - 1)], 100)
     m.update()
 
@@ -211,28 +228,6 @@ def run():
         print(str(N - 1) + ' '),
         for i in range(len(B)):
             print('%d ' % round(xx[label('x', i, N - 1)])),
-
-
-def foo():
-    m = g.Model("mip1")
-
-    # Create variables
-    x = m.addVar(vtype=g.GRB.BINARY, name="x")
-    y = m.addVar(vtype=g.GRB.BINARY, name="y")
-    z = m.addVar(vtype=g.GRB.BINARY, name="z")
-
-    # Integrate new variables
-    m.update()
-
-    # Set objective
-    m.setObjective(x + y + 2 * z, g.GRB.MAXIMIZE)
-
-    # Add constraint: x + 2 y + 3 z <= 4
-    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
-
-    m.optimize()
-
-    print(len(m.getConstrs()))
 
 
 if __name__ == '__main__':
